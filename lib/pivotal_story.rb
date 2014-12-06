15b2507c4558d4099f21e5cd1cd963e09dcff298
Edderic Ugaddan <edderic@gmail.com>
2014-12-06 16:04:11 -0500

    added pivotal story and unit tests

---
+++ lib/pivotal_story.rb
@@ -0,0 +1,32 @@
+#
+# Wrapper for a Pivotal Tracker story
+#
+# - id: story id
+# - project_id: found in the pivotal tracker settings page
+# - api_token: found in the PT settings page
+
+module Pivotal
+  class Story
+    def initialize(id, project_id, api_token)
+      @id = id
+      @project_id = project_id
+      @api_token = api_token
+    end
+
+    def api_version
+      "v5"
+    end
+
+    def get!
+      `#{get_request}`
+    end
+
+    def link
+      "'https://www.pivotaltracker.com/services/#{api_version}/projects/#{@project_id}/stories/#{@id}'"
+    end
+
+    def get_request
+      "curl -X GET -H 'X-TrackerToken: #{@api_token}' #{link}"
+    end
+  end
+end
---
+++ spec/pivotal_story_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/pivotal'
+
+describe 'Pivotal::Story' do
+  describe '#get!' do
+    it 'should execute the proper cURL string' do
+      story_id = '123'
+      project_id = '321'
+      api_token = 'abc10xyz'
+      pivotal_story = Pivotal::Story.new(story_id, project_id, api_token)
+      get_request = "curl -X GET -H 'X-TrackerToken: abc10xyz' 'https://www.pivotaltracker.com/services/v5/projects/321/stories/123'"
+      allow(pivotal_story).to receive(:`).with get_request
+
+      pivotal_story.get!
+
+      expect(pivotal_story).to have_received(:`).with get_request
+    end
+  end
+end
