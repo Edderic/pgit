b3ee95e791f1775c14e549849bf650a7b9ad6e44
Edderic Ugaddan <edderic@gmail.com>
2014-12-07 10:44:04 -0500

    refactored file names

--- lib/pivotal.rb
+++ lib/pivotal.rb
@@ -1,51 +1,11 @@
 require 'yaml'
 require 'json'
 require 'optparse'
-require_relative 'pgit_story'
-require_relative 'pgit_current_project'
-require_relative 'pgit_configuration'
-require_relative 'pgit_story_branch'
-require_relative 'pgit_name_parser'
-
-# TODO:
-# cf, config PATH
-#   - sets the config path
-#   - shows where the config file is if no argumets
-#
-#
-# sbr, story_branch
-#   -j, --join STORY_ID
-#     * will check the remotes
-#       * join if there's only one
-#       * ask which one to join if n > 1
-#       * if empty, ask if you mean 'start'
-#   -s, --start STORY_ID
-#     makes the parsed branch name and checkouts to it
-#
-#   -f, --finish
-#     Finish
-#       - fetch staging branch
-#       - rebase on top of staging
-#       - merge to local staging
-#       - remove old branch locally
-#       - remove old branch remotely
-#       - push staging to remote
-#       - push to heroku staging
-#   -e, --edit STORY_ATTRIBUTE ARGS
-#     * edits the story_attribute and sends a POST request
-# cm, commit
-#   * delegates to `git commit` but adds story-id
-#
-#
-# Epic flag
-# Start a story
-# Display ACs
-#   - done
-#   - not done
-#
-# move to existing branch (story_id)
-# Move to Next story (Assigned)
-# Move to Next story (Unassigned)
+require_relative 'pgit/story'
+require_relative 'pgit/current_project'
+require_relative 'pgit/configuration'
+require_relative 'pgit/story_branch'
+require_relative 'pgit/name_parser'
 
 module PGit
   class Application
