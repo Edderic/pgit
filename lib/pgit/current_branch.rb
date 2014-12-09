a3229f1d19cd519ad172cc98ef6a267128f846e9
Edderic Ugaddan <edderic@gmail.com>
2014-12-09 08:21:19 -0500

    added story_id method
    - also refactored current_branch. Creates instances now

--- lib/pgit/current_branch.rb
+++ lib/pgit/current_branch.rb
@@ -1,13 +1,23 @@
 module PGit
   class CurrentBranch
-    def self.name
-      branches = `git branch`
-      match = branches.scan(/\*.+/)
-      if match.any?
-        match.first.gsub(/\*\s*/, '')
-      else
-        raise branches
-      end
+    def initialize
+      @branches = `git branch`
+
+      raise @branches unless current.any?
+    end
+
+    def name
+      current.first.gsub(/\*\s*/, '')
+    end
+
+    def story_id
+      name.scan(/\d+$/).first
+    end
+
+    private
+
+    def current
+      @branches.scan(/\*.+/)
     end
   end
 end
--- spec/pgit/current_branch_spec.rb
+++ spec/pgit/current_branch_spec.rb
@@ -1,7 +1,7 @@
 require 'pgit'
 
 describe 'PGit::CurrentBranch' do
-  describe '.name' do
+  describe '#name' do
     it 'should return the current branch' do
       git_branch_command = 'git branch'
       fake_current_branch = 'master'
@@ -12,9 +12,45 @@ describe 'PGit::CurrentBranch' do
       FAKE_BRANCHES
 
       allow(PGit::CurrentBranch).to receive(:`).with(git_branch_command).and_return(fake_branches)
-      current_branch = PGit::CurrentBranch.name
+      current_branch_name = PGit::CurrentBranch.new.name
 
-      expect(current_branch).to eq fake_current_branch
+      expect(current_branch_name).to eq fake_current_branch
+    end
+  end
+
+  describe '#story_id' do
+    describe 'current branch has no story id' do
+      it 'should return nil' do
+        git_branch_command = 'git branch'
+        fake_current_branch = 'master'
+        fake_branches = <<-FAKE_BRANCHES
+        some-branch
+        * master
+        some-other-branch
+        FAKE_BRANCHES
+
+        allow_any_instance_of(PGit::CurrentBranch).to receive(:`).with(git_branch_command).and_return(fake_branches)
+        current_branch_story_id = PGit::CurrentBranch.new.story_id
+
+        expect(current_branch_story_id).to be_nil
+      end
+    end
+
+    describe 'current branch has story id' do
+      it 'should return the story id' do
+        git_branch_command = 'git branch'
+        fake_current_branch = 'master'
+        fake_branches = <<-FAKE_BRANCHES
+        some-branch
+        * some-feature-branch-123456
+        some-other-branch
+        FAKE_BRANCHES
+
+        allow_any_instance_of(PGit::CurrentBranch).to receive(:`).with(git_branch_command).and_return(fake_branches)
+        current_branch_story_id = PGit::CurrentBranch.new.story_id
+
+        expect(current_branch_story_id).to eq '123456'
+      end
     end
   end
 
@@ -27,9 +63,10 @@ describe 'PGit::CurrentBranch' do
           (or any of the parent directories): .git
       FAKE_BRANCHES
 
-      allow(PGit::CurrentBranch).to receive(:`).with(git_branch_command).and_return(fake_branches)
 
-      expect{PGit::CurrentBranch.name}.to raise_error(fake_branches)
+      allow_any_instance_of(PGit::CurrentBranch).to receive(:`).with(git_branch_command).and_return(fake_branches)
+
+      expect{PGit::CurrentBranch.new}.to raise_error(fake_branches)
     end
   end
 end
