a54d47f1f03b1a6f9c71b7cb4d3a91fd5de768df
Edderic Ugaddan <edderic@gmail.com>
2014-12-10 09:52:06 -0500

    updated PGit::NameParser interface
    - wraps PGit::Story so that PGit::StoryBranch implementation is cleaner

--- lib/pgit/name_parser.rb
+++ lib/pgit/name_parser.rb
@@ -1,6 +1,6 @@
 #
 #   Parses pivotal story titles and turns them into git branch names
-# - unparsed_name: title of the pivotal tracker story
+# - story.name: title of the pivotal tracker story
 #
 # - story_id: id of the pivotal tracker story
 #
@@ -9,11 +9,14 @@ module PGit
   class StoryBranch
     class NameParser
       def initialize(story)
-        @story_id = story.id
-        @unparsed_name = story.name
+        @story = story
       end
 
-      def parse
+      def story_id
+        @story.id
+      end
+
+      def name
         remove_fluff_words
         remove_extraneous_white_spaces
         remove_periods
@@ -25,30 +28,30 @@ module PGit
       private
 
       def remove_periods
-        @unparsed_name.gsub!('.', '')
+        @story.name.gsub!('.', '')
       end
 
       def remove_fluff_words
         fluff_words = %w{the on of}
-        fluff_words.each { |fluff_word| @unparsed_name.gsub!(/\b#{fluff_word}\b/i, '') }
+        fluff_words.each { |fluff_word| @story.name.gsub!(/\b#{fluff_word}\b/i, '') }
         remove_extraneous_white_spaces
       end
 
       def remove_extraneous_white_spaces
-        @unparsed_name.strip!
-        @unparsed_name.gsub!(/\s+/, ' ')
+        @story.name.strip!
+        @story.name.gsub!(/\s+/, ' ')
       end
 
       def replace_whitespace_with_dashes
-        @unparsed_name.gsub!(' ', '-')
+        @story.name.gsub!(' ', '-')
       end
 
       def downcase
-        @unparsed_name.downcase!
+        @story.name.downcase!
       end
 
       def add_story_id
-        "#{@unparsed_name}-#{@story_id}"
+        "#{@story.name}-#{story_id}"
       end
     end
   end
--- spec/pgit/story_branch/name_parser_spec.rb
+++ spec/pgit/story_branch/name_parser_spec.rb
@@ -1,16 +1,17 @@
 require 'pgit'
 
 describe 'PGit::StoryBranch::NameParser' do
-  describe '#parse' do
+  describe '#name' do
     it 'should remove fluff words, remove periods,
         replace spaces with dashes, suffix story_id' do
 
       unparsed_name = "Deemphasize the Clue on the stimulus screen of multi-screen exercises."
       story_id = '12345'
-      name_parser = PGit::StoryBranch::NameParser.new(unparsed_name, story_id)
+      fake_story = double('PGit::Story', id: story_id, name: unparsed_name)
 
-      puts name_parser.methods(false)
-      parsed = name_parser.parse
+      name_parser = PGit::StoryBranch::NameParser.new(fake_story)
+      parsed = name_parser.name
+
       expect(parsed).to eq "deemphasize-clue-stimulus-screen-multi-screen-exercises-12345"
     end
 
