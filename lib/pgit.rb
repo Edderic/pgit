34eb641a8817786d85cdb6ed504c924e9004d330
Edderic Ugaddan <edderic@gmail.com>
2014-12-11 21:56:10 -0500

    reordered dependency list. Easier to parse.

--- lib/pgit.rb
+++ lib/pgit.rb
@@ -1,13 +1,14 @@
-require 'pgit/version.rb'
-require 'pgit/story'
+require 'json'
+require 'pgit/configuration'
 require 'pgit/current_branch'
 require 'pgit/current_project'
-require 'pgit/configuration'
+require 'pgit/installer'
+require 'pgit/name_parser'
+require 'pgit/story'
 require 'pgit/story_branch'
 require 'pgit/story_branch/application'
-require 'pgit/name_parser'
+require 'pgit/version.rb'
 require 'yaml'
-require 'json'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
