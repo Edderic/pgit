513e35fc12baa746f53699148a9c11c3bc9aeaaf
Edderic Ugaddan <edderic@gmail.com>
2014-12-11 20:42:05 -0500

    refactored. also added a reminder to edit the example config file

--- lib/pgit/installer.rb
+++ lib/pgit/installer.rb
@@ -15,21 +15,27 @@ module PGit
       puts  "*** Installing example pgit configuration file under ~/.pgit.rc.yml. Continue? [Y/n]"
       if STDIN.gets.chomp.match(/y/i)
         puts "Saving example pgit config in ~/.pgit.rc.yml..."
-        File.open(@expanded_path, 'w') do |f|
-          f.puts "---"
-          f.puts "projects:"
-          f.puts "  - path: ~/some/path/to/a/pivotal-git/project"
-          f.puts "    id: 12345"
-          f.puts "    api_token: somepivotalatoken124"
-
-          f.puts "  - path: ~/some/other/pivotal-git/project"
-          f.puts "    id: 23429070"
-          f.puts "    api_token: somepivotalatoken124"
-          f.close
-        end
+        write_example_pgit_rc_file
       else
         puts "Aborting installation..."
       end
+    end
+
+    def write_example_pgit_rc_file
+      File.open(@expanded_path, 'w') do |f|
+        f.puts "---"
+        f.puts "projects:"
+        f.puts "  - path: ~/some/path/to/a/pivotal-git/project"
+        f.puts "    id: 12345"
+        f.puts "    api_token: somepivotalatoken124"
+
+        f.puts "  - path: ~/some/other/pivotal-git/project"
+        f.puts "    id: 23429070"
+        f.puts "    api_token: somepivotalatoken124"
+        f.close
+      end
+
+      puts "Saved! Please edit ~/.pgit.rc.yml and add the proper Pivotal Tracker API tokens, id, and file paths for each project"
     end
   end
 end
--- spec/pgit/installer_spec.rb
+++ spec/pgit/installer_spec.rb
@@ -32,6 +32,7 @@ describe 'PGit::Installer' do
       message = "*** Installing example pgit configuration file under ~/.pgit.rc.yml. " +
         "Continue? [Y/n]"
       confirmation_message = "Saving example pgit config in ~/.pgit.rc.yml..."
+      edit_message = "Saved! Please edit ~/.pgit.rc.yml and add the proper Pivotal Tracker API tokens, id, and file paths for each project"
       answer = instance_double("String", chomp: 'y')
       expanded_path = "/home/edderic/.pgit.rc.yml"
       fake_writable_file = double('File')
@@ -41,6 +42,7 @@ describe 'PGit::Installer' do
       allow(STDIN).to receive(:gets).and_return(answer)
       allow_any_instance_of(PGit::Installer).to receive(:puts).with(message)
       allow_any_instance_of(PGit::Installer).to receive(:puts).with(confirmation_message)
+      allow_any_instance_of(PGit::Installer).to receive(:puts).with(edit_message)
       installer = PGit::Installer.new(global_opts, opts, args)
 
       expect(installer).to have_received(:puts).with(message)
@@ -56,10 +58,12 @@ describe 'PGit::Installer' do
           "Continue? [Y/n]"
         save_message = "Saving example pgit config in ~/.pgit.rc.yml..."
         expanded_path = "/home/edderic/.pgit.rc.yml"
+        edit_message = "Saved! Please edit ~/.pgit.rc.yml and add the proper Pivotal Tracker API tokens, id, and file paths for each project"
         fake_writable_file = double('File')
         allow(File).to receive(:expand_path).with("~/.pgit.rc.yml").and_return(expanded_path)
         allow(File).to receive(:exists?).with(expanded_path).and_return(false)
         allow(File).to receive(:open).with(expanded_path, 'w').and_return(fake_writable_file)
+        allow_any_instance_of(PGit::Installer).to receive(:puts).with(edit_message)
         allow_any_instance_of(PGit::Installer).to receive(:puts).with(first_message)
         allow_any_instance_of(PGit::Installer).to receive(:puts).with(save_message)
 
@@ -81,11 +85,13 @@ describe 'PGit::Installer' do
         save_message = "Saving example pgit config in ~/.pgit.rc.yml..."
         expanded_path = "/home/edderic/.pgit.rc.yml"
         fake_writable_file = double('File')
+        edit_message = "Saved! Please edit ~/.pgit.rc.yml and add the proper Pivotal Tracker API tokens, id, and file paths for each project"
         allow(File).to receive(:expand_path).with("~/.pgit.rc.yml").and_return(expanded_path)
         allow(File).to receive(:exists?).with(expanded_path).and_return(false)
         allow(File).to receive(:open).with(expanded_path, 'w').and_return(fake_writable_file)
         allow_any_instance_of(PGit::Installer).to receive(:puts).with(first_message)
         allow_any_instance_of(PGit::Installer).to receive(:puts).with(save_message)
+        allow_any_instance_of(PGit::Installer).to receive(:puts).with(edit_message)
 
         answer = instance_double("String", chomp: 'Y')
         allow(STDIN).to receive(:gets).and_return(answer)
@@ -93,6 +99,32 @@ describe 'PGit::Installer' do
         installer = PGit::Installer.new(global_opts, opts, args)
 
         expect(File).to have_received(:open).with(expanded_path, 'w')
+      end
+
+      it 'should ask the user to edit the config file' do
+        global_opts = {}
+        opts = { }
+        args = {}
+
+        first_message = "*** Installing example pgit configuration file under ~/.pgit.rc.yml. " +
+          "Continue? [Y/n]"
+        save_message = "Saving example pgit config in ~/.pgit.rc.yml..."
+        edit_message = "Saved! Please edit ~/.pgit.rc.yml and add the proper Pivotal Tracker API tokens, id, and file paths for each project"
+        expanded_path = "/home/edderic/.pgit.rc.yml"
+        fake_writable_file = double('File')
+        allow(File).to receive(:expand_path).with("~/.pgit.rc.yml").and_return(expanded_path)
+        allow(File).to receive(:exists?).with(expanded_path).and_return(false)
+        allow(File).to receive(:open).with(expanded_path, 'w').and_return(fake_writable_file)
+        allow_any_instance_of(PGit::Installer).to receive(:puts).with(first_message)
+        allow_any_instance_of(PGit::Installer).to receive(:puts).with(edit_message)
+        allow_any_instance_of(PGit::Installer).to receive(:puts).with(save_message)
+
+        answer = instance_double("String", chomp: 'Y')
+        allow(STDIN).to receive(:gets).and_return(answer)
+
+        installer = PGit::Installer.new(global_opts, opts, args)
+
+        expect(installer).to have_received(:puts).with(edit_message)
       end
     end
 
