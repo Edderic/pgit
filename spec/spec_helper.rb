4a5be6a51b068ae3632a5a707b1020da39307e9b
Edderic Ugaddan <edderic@gmail.com>
2014-12-13 23:56:22 -0500

    [#84531676] add coveralls gem; track test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'simplecov'
-SimpleCov.start
+require 'coveralls'
+Coveralls.wear!
 
 require 'pgit'
 
