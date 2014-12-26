9c96544681670bdf695b46e22add4aa82bdbd96e
Edderic Ugaddan <edderic@gmail.com>
2014-12-26 12:11:52 -0500

    [#84572026] execute commands when commands exist

--- lib/pgit/command/application.rb
+++ lib/pgit/command/application.rb
@@ -1,26 +1,36 @@
 module PGit
   class Command
     class Application
-      attr_reader :commands
+      attr_reader :commands, :opts
       def initialize(global_opts, opts, args)
         config = PGit::Configuration.new
         current_project = PGit::CurrentProject.new(config.to_yaml)
-        @commands = current_project.commands
+        @opts = opts
+        @commands = []
+        current_project.commands.each do |c|
+          c.each do |name, steps|
+            @commands << PGit::Command.new(name, steps)
+          end
+        end
 
         if opts[:list]
           list
+        elsif opts[:execute]
+          execute
         end
       end
 
       def list
         puts "Listing custom commands of the current project..."
         puts
-        commands.each do |c|
-          c.each do |name, steps|
-            command = PGit::Command.new(name, steps)
-            puts command.to_s
-          end
+        @commands.each do |c|
+          puts c.to_s
         end
+      end
+
+      def execute
+        s = @commands.find{|c| c.name == opts[:execute] }
+        s.execute
       end
     end
   end
