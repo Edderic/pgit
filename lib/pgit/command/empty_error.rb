60fcb1702d7be2248ed9353fdba1b83fe92418b9
Edderic Ugaddan <edderic@gmail.com>
2014-12-27 06:48:57 -0500

    [#84572026] changed EmptyError message to be less verbose.

--- lib/pgit/command/empty_error.rb
+++ lib/pgit/command/empty_error.rb
@@ -2,7 +2,7 @@ module PGit
   class Command
     class EmptyError < PGit::Error
       def initialize
-        @message = "No commands are listed for this project. Run `pgit command --add --name='command_name' --steps='step 1, step2'` to add a command for this project. You can also edit ~/.pgit.rc.yml directly"
+        @message = "No commands are listed for this project. Run `pgit command add --help` for more info."
         super @message
       end
     end
--- spec/pgit/command/empty_error_spec.rb
+++ spec/pgit/command/empty_error_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'PGit::Command::EmptyError' do
   it 'gives instructions' do
     empty_error = PGit::Command::EmptyError.new
-    expected_message = "No commands are listed for this project. Run `pgit command --add --name='command_name' --steps='step 1, step2'` to add a command for this project. You can also edit ~/.pgit.rc.yml directly"
+    expected_message = "No commands are listed for this project. Run `pgit command add --help` for more info."
     message = empty_error.message
 
     expect(message).to eq expected_message
