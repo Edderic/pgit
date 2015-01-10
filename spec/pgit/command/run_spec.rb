6b53bf6f15975c2088f24cb41968fc786818ac8b
Edderic Ugaddan <edderic@gmail.com>
2015-01-10 14:06:02 -0500

    [#85837942] refactored to use PGit::Command::Application in the PGit::Command::Run test

--- spec/pgit/command/run_spec.rb
+++ spec/pgit/command/run_spec.rb
@@ -7,28 +7,8 @@ describe 'PGit::Command::Run' do
       opts = []
       args = ['non_existent_command']
 
-      expected_message = "Listing custom commands of the current project..."
-
-      fake_name = 'finish'
-      fake_steps = ["git fetch origin master",
-                    "git push origin :STORY_BRANCH"]
-      fake_commands = { fake_name => fake_steps }
-      fake_command_string = double('fake_command_string')
-      fake_command = instance_double('PGit::Command', to_s: fake_command_string, name: 'finish')
-      fake_yaml = double('fake_yaml')
-      fake_configuration = instance_double('PGit::Configuration', to_yaml: fake_yaml)
-      fake_current_project = instance_double('PGit::CurrentProject',
-                                             commands: fake_commands)
-
-      allow(PGit::Configuration).to receive(:new).
-        and_return(fake_configuration)
-      allow(PGit::CurrentProject).to receive(:new).
-        with(fake_yaml).and_return(fake_current_project)
-      allow(PGit::Command).to receive(:new).
-        with(fake_name, fake_steps).and_return(fake_command)
-      allow_any_instance_of(PGit::Command::Run).
-        to receive(:puts)
-      allow(fake_command).to receive(:execute)
+      allow_any_instance_of(PGit::Command::Application).
+        to receive(:command).and_return(nil)
       app = PGit::Command::Run.new(global_opts, opts, args)
 
       expect do
@@ -37,33 +17,14 @@ describe 'PGit::Command::Run' do
     end
 
     it 'calls execute on the command if it exists' do
-      fake_name = 'finish'
-      global_opts = []
+      global_opts = {}
       opts = []
-      args = [fake_name]
+      args = ['existent_command']
 
-      expected_message = "Listing custom commands of the current project..."
-
-      fake_steps = ["git fetch origin master",
-                    "git push origin :STORY_BRANCH"]
-      fake_commands = { fake_name => fake_steps }
-      fake_command_string = double('fake_command_string')
-      fake_command = instance_double('PGit::Command', to_s: fake_command_string, name: 'finish')
-      fake_yaml = double('fake_yaml')
-      fake_configuration = instance_double('PGit::Configuration', to_yaml: fake_yaml)
-      fake_current_project = instance_double('PGit::CurrentProject',
-                                             commands: fake_commands)
-
-      allow(PGit::Configuration).to receive(:new).
-        and_return(fake_configuration)
-      allow(PGit::CurrentProject).to receive(:new).
-        with(fake_yaml).and_return(fake_current_project)
-      allow(PGit::Command).to receive(:new).
-        with(fake_name, fake_steps).and_return(fake_command)
-      allow_any_instance_of(PGit::Command::Run).
-        to receive(:puts)
+      fake_command = instance_double('PGit::Command')
       allow(fake_command).to receive(:execute)
-
+      allow_any_instance_of(PGit::Command::Application).
+        to receive(:command).and_return(fake_command)
       app = PGit::Command::Run.new(global_opts, opts, args)
       app.execute!
 
