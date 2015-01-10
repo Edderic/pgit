7ae46b79ea3c7c4ec3e5565493f87007ee9493c4
Edderic Ugaddan <edderic@gmail.com>
2015-01-10 14:24:38 -0500

    [#85837942] reduced duplication further by stubbing out PGit::Command::Application on PGit::CurrentProject::Command::Show

--- spec/pgit/current_project/command/show_spec.rb
+++ spec/pgit/current_project/command/show_spec.rb
@@ -3,63 +3,35 @@ require 'spec_helper'
 describe 'PGit::CurrentProject::Command::Show' do
   describe 'without any options' do
     it 'raises an error if there are no commands' do
-      global_opts = {}
-      opts = { list: true }
-      args = {}
+      global_opts = []
+      opts = []
+      args = []
 
-      expected_message = "Listing custom commands of the current project..."
-
-      fake_commands = []
-      fake_yaml = double('fake_yaml')
-      fake_configuration = instance_double('PGit::Configuration', to_yaml: fake_yaml)
-      fake_current_project = instance_double('PGit::CurrentProject',
-                                             commands: fake_commands)
-
-      allow(PGit::Configuration).to receive(:new).
-        and_return(fake_configuration)
-      allow(PGit::CurrentProject).to receive(:new).
-        with(fake_yaml).and_return(fake_current_project)
-      allow_any_instance_of(PGit::CurrentProject::Command::Show).
-        to receive(:puts)
-
+      allow_any_instance_of(PGit::Command::Application).
+        to receive(:commands).and_return([])
       app = PGit::CurrentProject::Command::Show.new(global_opts, opts, args)
-      expect do
-        app.execute!
-      end.to raise_error PGit::Command::EmptyError
+
+      expect { app.execute! }.to raise_error PGit::Command::EmptyError
     end
 
     it 'lists the commands if there are commands' do
-      global_opts = {}
-      opts = { list: true }
-      args = {}
+      global_opts = []
+      opts = []
+      args = ['fake_command']
 
       expected_message = "Listing custom commands of the current project..."
+      fake_formatted_command = instance_double('String')
+      fake_command = instance_double('PGit::Command', to_s: fake_formatted_command)
 
-      fake_name = 'finish'
-      fake_steps = ["git fetch origin master",
-                    "git push origin :STORY_BRANCH"]
-      fake_commands = { fake_name => fake_steps }
-      fake_command_string = double('fake_command_string')
-      fake_command = instance_double('PGit::Command', to_s: fake_command_string)
-      fake_yaml = double('fake_yaml')
-      fake_configuration = instance_double('PGit::Configuration', to_yaml: fake_yaml)
-      fake_current_project = instance_double('PGit::CurrentProject',
-                                             commands: fake_commands)
-
-      allow(PGit::Configuration).to receive(:new).
-        and_return(fake_configuration)
-      allow(PGit::CurrentProject).to receive(:new).
-        with(fake_yaml).and_return(fake_current_project)
-      allow(PGit::Command).to receive(:new).
-        with(fake_name, fake_steps).and_return(fake_command)
-      allow_any_instance_of(PGit::CurrentProject::Command::Show).
-        to receive(:puts)
+      allow_any_instance_of(PGit::Command::Application).
+        to receive(:commands).and_return([fake_command])
+      allow_any_instance_of(PGit::Command::Application).to receive(:puts)
       app = PGit::CurrentProject::Command::Show.new(global_opts, opts, args)
 
       app.execute!
 
       expect(app).to have_received(:puts).with(expected_message)
-      expect(app).to have_received(:puts).with(fake_command_string)
+      expect(app).to have_received(:puts).with(fake_formatted_command)
     end
   end
 end
