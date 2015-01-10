82a34f98bae8aa8494c3f18a5a9aba9845943199
Edderic Ugaddan <edderic@gmail.com>
2015-01-10 15:25:34 -0500

    [#85419112] `pgit command show` can take a command argument now

--- lib/pgit/current_project/command/show.rb
+++ lib/pgit/current_project/command/show.rb
@@ -6,11 +6,35 @@ module PGit
       class Show < PGit::Command::Application
         def execute!
           raise PGit::Command::EmptyError if commands.empty?
+
+          if search.empty?
+            show_all_of_current_project
+          else
+            show_one
+          end
+        end
+
+        def search
+          args.first || []
+        end
+
+        private
+
+        def show_all_of_current_project
           puts "Listing custom commands of the current project..."
           puts
-          commands.each do |c|
-            puts c.to_s
-          end
+          commands.each { |c| puts c.to_s }
+          puts
+        end
+
+        def show_one
+          raise PGit::Command::UserError,
+            "Command '#{search}' not found for this project" unless command
+
+          puts "Listing custom command '#{command.name}' of the current project..."
+          puts
+          puts command.to_s
+          puts
         end
       end
     end
--- spec/pgit/current_project/command/show_spec.rb
+++ spec/pgit/current_project/command/show_spec.rb
@@ -17,7 +17,7 @@ describe 'PGit::CurrentProject::Command::Show' do
     it 'lists the commands if there are commands' do
       global_opts = []
       opts = []
-      args = ['fake_command']
+      args = []
 
       expected_message = "Listing custom commands of the current project..."
       fake_formatted_command = instance_double('String')
@@ -32,6 +32,52 @@ describe 'PGit::CurrentProject::Command::Show' do
 
       expect(app).to have_received(:puts).with(expected_message)
       expect(app).to have_received(:puts).with(fake_formatted_command)
+    end
+  end
+
+  describe 'when a command_name is passed in' do
+    it 'should print the command if exists' do
+      existent_command_name = 'fake_command'
+      global_opts = []
+      opts = []
+      args = [existent_command_name]
+
+      expected_message = "Listing custom command '#{existent_command_name}' of the current project..."
+      fake_formatted_command = instance_double('String')
+      fake_command = instance_double('PGit::Command',
+                                     to_s: fake_formatted_command,
+                                     name: existent_command_name)
+
+      allow_any_instance_of(PGit::Command::Application).
+        to receive(:commands).and_return([fake_command])
+      allow_any_instance_of(PGit::Command::Application).to receive(:puts)
+      app = PGit::CurrentProject::Command::Show.new(global_opts, opts, args)
+
+      app.execute!
+
+      expect(app).to have_received(:puts).with(expected_message)
+      expect(app).to have_received(:puts).with(fake_formatted_command)
+    end
+
+    it 'should raise an error if the command does not exist' do
+      nonexistent_command_name = 'non_existent_fake_command'
+      existent_command_name = 'fake_command'
+      global_opts = []
+      opts = []
+      args = [nonexistent_command_name]
+
+      expected_message = "Command '#{nonexistent_command_name}' not found for this project"
+      fake_formatted_command = instance_double('String')
+      fake_command = instance_double('PGit::Command',
+                                     to_s: fake_formatted_command,
+                                     name: existent_command_name)
+
+      allow_any_instance_of(PGit::Command::Application).
+        to receive(:commands).and_return([fake_command])
+      allow_any_instance_of(PGit::Command::Application).to receive(:puts)
+      app = PGit::CurrentProject::Command::Show.new(global_opts, opts, args)
+
+      expect{ app.execute! }.to raise_error PGit::Command::UserError, expected_message
     end
   end
 end
