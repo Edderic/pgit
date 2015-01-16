efb018ac660d5727fbf383b1ca0e34c20f234d38
Edderic Ugaddan <edderic@gmail.com>
2015-01-16 07:53:57 -0500

    [#85484698] removed layout error. No longer needed.

--- lib/pgit/configuration/layout_error.rb
+++
@@ -1,9 +0,0 @@
-module PGit
-  class Configuration
-    class LayoutError < PGit::Error
-      def prepend_general_message(message)
-        "#{message}\nPlease have the following layout:\n" + YAML.dump(PGit::Configuration.default_options)
-      end
-    end
-  end
-end
