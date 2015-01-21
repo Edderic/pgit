64b04224dde4b124116cce2f9e511d15bac4260c
Edderic Ugaddan <edderic@gmail.com>
2015-01-21 08:29:08 -0500

    [#86508826] extracted PGit::Command::Remove

---
+++ lib/pgit/command/remove.rb
@@ -0,0 +1,38 @@
+require 'forwardable'
+
+module PGit
+  class Command
+    class Remove
+      extend Forwardable
+      def_delegators :@app, :commands, :args, :global_opts, :opts
+      def_delegators :@command, :name
+
+      attr_reader :command
+      def initialize(app)
+        @app = app
+        @command = PGit::Command.new(opts.fetch(:name), opts.fetch(:steps){ ['fake_steps'] })
+      end
+
+      def execute!
+        check_command_exists_for_remove
+
+        command.remove!
+        display_success_msg
+      end
+
+      private
+
+      def exists?
+        commands.find { |c| c.name == name }
+      end
+
+      def display_success_msg
+        puts "Successfully removed command '#{name}' from the current project!"
+      end
+
+      def check_command_exists_for_remove
+        raise PGit::Error::User.new "Cannot remove a command that does not exist in the current project. See `pgit command add --help` if you want to add a new command" unless exists?
+      end
+    end
+  end
+end
---
+++ spec/pgit/command/remove_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe 'PGit::Command::Remove' do
+  describe 'when the command already exists' do
+    it 'should be successful' do
+      name = 'existent_command'
+      steps = ['existent_step']
+      global_opts = {}
+      opts = { name: name }
+      args = []
+      expected_message = "Successfully removed command 'existent_command' from the current project!"
+      fake_command = instance_double('PGit::Command', name: name,
+                                                      steps: steps,
+                                                      save: nil,
+                                                      remove!: nil)
+
+      fake_app = instance_double('PGit::Command::Application',
+                                 commands: [fake_command],
+                                 args: args,
+                                 opts: opts,
+                                 global_opts: global_opts)
+
+      allow(PGit::Command).to receive(:new).with(name, ['fake_steps']).and_return(fake_command)
+      remove = PGit::Command::Remove.new(fake_app)
+      allow(remove).to receive(:puts).with(expected_message)
+      remove.execute!
+
+      expect(remove).to have_received(:puts).with(expected_message)
+    end
+  end
+
+  describe 'when there the command does not exist' do
+    it 'should raise an error' do
+      non_existent_name = 'non_existent_command'
+      non_existent_steps = ['existent_step']
+      existent_name = 'existent_name'
+      existent_steps= 'existent_steps'
+      global_opts = {}
+      opts = { name: non_existent_name }
+      args = []
+      expected_message = "Cannot remove a command that does not exist in the current project. See `pgit command add --help` if you want to add a new command"
+      fake_command = instance_double('PGit::Command',
+                                     name: existent_name,
+                                     steps: existent_steps,
+                                     save: nil)
+
+      new_fake_command = instance_double('PGit::Command',
+                                     name: non_existent_name,
+                                     steps: non_existent_steps,
+                                     save: nil)
+
+      allow(PGit::Command).to receive(:new).with(opts[:name], ['fake_steps']).and_return(new_fake_command)
+
+      fake_app = instance_double('PGit::Command::Application',
+                                 commands: [fake_command],
+                                 args: args,
+                                 opts: opts,
+                                 global_opts: global_opts)
+
+      remove = PGit::Command::Remove.new(fake_app)
+
+      expect{ remove.execute! }.to raise_error(PGit::Error::User, expected_message)
+    end
+  end
+end
