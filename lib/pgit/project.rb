1a9439b68c01d87e59dae395f6120e0f265b1a29
Edderic Ugaddan <edderic@gmail.com>
2015-02-05 08:37:53 -0500

    [#85419228] PGit::Project #new, #to_hash, and setters
    - it can be initialized two ways: regular and block
    
    - block is good when the project does not exist in the configuration
      file.
    
    - regular initialization (without block) is good for when the project
      does exist in the configuration file.
    
    - to_hash correctly returns the hash equivalent.

--- lib/pgit/project.rb
+++ lib/pgit/project.rb
@@ -1,25 +1,42 @@
+require 'pgit'
+
 module PGit
   class Project
-    attr_reader :path, :api_token
-    [:path, :api_token].each do |method_name|
-      define_method "has_#{method_name}?=" do |val|
-        instance_variable_get(method_name) == val
+    attr_reader :path, :api_token, :id, :commands
+    def initialize(configuration=:no_config_provided,
+                   project_hash=:no_proj_hash_provided,
+                   &block)
+      proj = Hash.new
+      block_given? ? yield(proj) : proj = project_hash
+
+      @configuration = configuration
+      @path = proj.fetch('path') { Dir.pwd }
+      @api_token = proj.fetch('api_token')
+      @id = proj.fetch('id')
+      @commands = build_commands(proj.fetch('commands') { Array.new })
+    end
+
+    [:id, :path, :api_token].each do |method_name|
+      define_method "has_#{method_name}?" do |val|
+        instance_variable_get("@#{method_name}") == val
       end
     end
 
-    def initialize(project_hash)
-      @path = project_hash.fetch('path')
-      @api_token = project_hash.fetch('api_token')
+    def to_hash
+      {
+        "path" => path,
+        "api_token" => api_token,
+        "id" => id,
+        "commands" => @commands.map {|cmd| puts cmd.to_hash; cmd.to_hash}
+      }
     end
 
+    private
 
-
-    # def has_path?(some_path)
-      # path == some_path
-    # end
-#
-    # def has_api_token?(some_api_token)
-      # api_token == some_api_token
-    # end
+    def build_commands(cmds)
+      cmds.map do |cmd|
+        cmd.map { |k,v| PGit::Command.new(k, v, self) }.first
+      end
+    end
   end
 end
--- spec/pgit/project_spec.rb
+++ spec/pgit/project_spec.rb
@@ -1,14 +1,57 @@
 require 'spec_helper'
 
 describe 'PGit::Project' do
+  describe '#commands' do
+    it 'returns the commands if they exist' do
+      command = instance_double('PGit::Command')
+      command_name = 'command_name'
+      command_steps = ['subcommand1', 'subcommand2']
+      project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
+                        "id" => 12345,
+                        "api_token" => "astoeuh",
+                        "commands" => [{'command_name' => command_steps }] }
+      allow(PGit::Command).to receive(:new).and_return(command)
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
+
+      expect(proj.commands.first).to eq command
+    end
+
+    it 'returns empty array if there are no commands' do
+      project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
+                        "id" => 12345,
+                        "api_token" => "astoeuh",
+                        "commands" => [] }
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
+
+      expect(proj.commands).to eq []
+    end
+  end
+
   describe '#path' do
     it 'returns the path' do
       project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
                         "id" => 12345,
                         "api_token" => "astoeuh",
-                        "commands" => {} }
-      proj = PGit::Project.new(project_hash)
+                        "commands" => [] }
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
       expect(proj.path).to eq project_hash.fetch('path')
+    end
+
+    it 'defaults to Dir.pwd' do
+      configuration = instance_double('PGit::Configuration')
+      fake_path = double('path')
+      api_token = 's3cret'
+      id = 12345
+      allow(Dir).to receive(:pwd).and_return(fake_path)
+      proj = PGit::Project.new(configuration) do |p|
+        p["api_token"] = api_token
+        p["id"] = id
+      end
+
+      expect(proj.path).to eq fake_path
     end
   end
 
@@ -17,9 +60,22 @@ describe 'PGit::Project' do
       project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
                         "id" => 12345,
                         "api_token" => "astoeuh",
-                        "commands" => {} }
-      proj = PGit::Project.new(project_hash)
+                        "commands" => [] }
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
       expect(proj.api_token).to eq project_hash.fetch('api_token')
+    end
+  end
+
+  describe '#id' do
+    it 'returns the id' do
+      project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
+                        "id" => 12345,
+                        "api_token" => "astoeuh",
+                        "commands" => [] }
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
+      expect(proj.id).to eq project_hash.fetch('id')
     end
   end
 
@@ -28,10 +84,9 @@ describe 'PGit::Project' do
       project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
                         "id" => 12345,
                         "api_token" => "astoeuh",
-                        "commands" => {} }
-      proj = PGit::Project.new(project_hash)
-      puts "proj.methods(false)"
-      puts proj.methods(false)
+                        "commands" => [] }
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
 
       expect(proj).to be_has_path(project_hash.fetch('path'))
     end
@@ -40,8 +95,9 @@ describe 'PGit::Project' do
       project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
                         "id" => 12345,
                         "api_token" => "astoeuh",
-                        "commands" => {} }
-      proj = PGit::Project.new(project_hash)
+                        "commands" => [] }
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
 
       expect(proj).not_to be_has_path('some/other/path')
     end
@@ -52,8 +108,9 @@ describe 'PGit::Project' do
       project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
                         "id" => 12345,
                         "api_token" => "astoeuh",
-                        "commands" => {} }
-      proj = PGit::Project.new(project_hash)
+                        "commands" => [] }
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
 
       expect(proj).to be_has_api_token(project_hash.fetch('api_token'))
     end
@@ -62,19 +119,77 @@ describe 'PGit::Project' do
       project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
                         "id" => 12345,
                         "api_token" => "astoeuh",
-                        "commands" => {} }
-      proj = PGit::Project.new(project_hash)
+                        "commands" => [] }
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
 
       expect(proj).not_to be_has_api_token('some/other/api_token')
     end
   end
 
   describe '#has_id?(id)' do
+    it 'returns true if the ids are the same' do
+      project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
+                        "id" => 12345,
+                        "api_token" => "astoeuh",
+                        "commands" => [] }
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
+
+      expect(proj).to be_has_id(project_hash.fetch('id'))
+    end
+
+    it 'returns false if the ids are not the same' do
+      project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
+                        "id" => 12345,
+                        "api_token" => "astoeuh",
+                        "commands" => [] }
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
+
+      expect(proj).not_to be_has_id(54321)
+    end
   end
 
-  describe '#commands' do
+  describe 'instatiating with a block' do
+    it 'sets the api_token, path, id based on the block' do
+      path = '/some/path'
+      api_token = 's3cret'
+      id = 12345
+      commands = []
+      configuration = instance_double('PGit::Configuration')
+
+      proj = PGit::Project.new(configuration) do |p|
+        p["path"] = path
+        p["api_token"] = api_token
+        p["id"] = id
+        p["commands"] = commands
+      end
+
+      expect(proj.path).to eq path
+      expect(proj.api_token).to eq api_token
+      expect(proj.id).to eq id
+      expect(proj.commands).to eq commands
+    end
   end
 
   describe '#to_hash' do
+    it 'returns the hash version' do
+      command = "command1"
+      steps = ["step1", "step2"]
+      command_hash = { command => steps }
+      project_hash =  { "path" => "/Therapy-Exercises-Online/some_other_project",
+                        "id" => 12345,
+                        "api_token" => "astoeuh",
+                        "commands" => [
+                          command_hash
+                        ]
+                      }
+      fake_command = instance_double('PGit::Command', to_hash: command_hash)
+      configuration = instance_double('PGit::Configuration')
+      proj = PGit::Project.new(configuration, project_hash)
+
+      expect(proj.to_hash).to eq project_hash
+    end
   end
 end
