89c3865ea8de49846976a8171cc8bf97979acb6e
Edderic Ugaddan <edderic@gmail.com>
2015-02-08 14:06:50 -0500

    [#85419228] PGit::CurrentProject makes use of existing machinery in PGit::Project

--- lib/pgit/current_project.rb
+++ lib/pgit/current_project.rb
@@ -1,99 +1,18 @@
-#
-#   Decides what the "current project" is, in relation to the pwd.
-#
-# - config_yaml: has the project configurations. It has at least one
-#   project. Each project has an (pivotal) api_token, path, and (pivotal)
-#   id
-#
+require 'pgit'
+require 'forwardable'
 
 module PGit
   class CurrentProject
-    attr_accessor :commands
-    attr_reader :index, :configuration
+    extend Forwardable
+
+    PGit::Project.instance_methods(false).each do |m|
+      def_delegator :@current, m
+    end
+
     def initialize(configuration)
-      @configuration = configuration
-      config_yaml = configuration.to_yaml
-      @current_project = find_current_project(config_yaml)
-      config_yaml["projects"].each_with_index do |project, index|
-        @index = index if project["path"].match(path)
+      @current = configuration.projects.find do |p|
+        File.expand_path(p.path) == Dir.pwd
       end
-
-      @commands = @current_project["commands"] || {}
-    end
-
-    def path
-      @current_project["path"]
-    end
-
-    def id
-      @current_project["id"]
-    end
-
-    def api_token
-      @current_project["api_token"]
-    end
-
-    def to_hash
-      {
-        "id" => id,
-        "api_token" => api_token,
-        "path" => path,
-        "commands" => commands
-      }
-    end
-
-    def save(saveable)
-      save_or_remove do |projects|
-        projects[index] = @current_project.merge(saveable.to_save)
-      end
-    end
-
-    def remove!(removable)
-      save_or_remove do |projects|
-        send(removable.project_key).reject! { |k,v| k == removable.name }
-        projects[index] = @current_project
-      end
-    end
-
-    def to_h
-      to_hash
-    end
-
-    private
-
-    def save_or_remove
-      projects = configuration.projects
-
-      yield projects
-
-      configuration.projects = projects
-      configuration.save
-    end
-
-    def escape_slashes(project_path)
-      project_path.gsub('/','\/')
-    end
-
-    def find_matching_projects(projects)
-      projects.select do |project|
-        project_path = project["path"]
-        extended_path = File.expand_path(project_path, __FILE__)
-        escaped_project = escape_slashes(extended_path)
-        Dir.pwd.match(/#{escaped_project}/i)
-      end
-    end
-
-    def find_current_project(config_yaml)
-      projects = config_yaml["projects"]
-      matching_projects = find_matching_projects(projects)
-
-      PGit::CurrentProject::Validator.new(matching_projects)
-      find_best_match(matching_projects)
-    end
-
-    def find_best_match(matching_projects)
-      matching_projects.sort! { |a,b| b["path"].length <=> a["path"].length }
-      matching_projects.first
     end
   end
 end
