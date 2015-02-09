fb666e58800464469b96024731baaddd1d3b98a8
Edderic Ugaddan <edderic@gmail.com>
2015-02-09 07:12:17 -0500

    [#85419228] made instatiation of PGit::Command a responsibiilty of PGit::Command::Run instances.
    Also updated error messages

--- lib/pgit/command/run.rb
+++ lib/pgit/command/run.rb
@@ -4,14 +4,16 @@ module PGit
   class Command
     class Run
       extend Forwardable
-      def_delegators :@app, :commands, :args, :global_opts, :opts
+      def_delegators :@app, :commands, :args, :global_opts, :opts, :current_project
+      attr_reader :command
 
       def initialize(app)
         @app = app
+        @command = PGit::Command.new(search, opts.fetch(:steps) { :no_steps_provided }, current_project)
       end
 
       def execute!
-        error_message = "Command '#{search}' does not exist. Run 'pgit command show' to see the available custom commands."
+        error_message = "Command '#{search}' does not exist. Run 'pgit cmd show' to see the available custom commands."
         raise PGit::Error::User, error_message unless command
 
         command.execute
--- spec/pgit/command/run_spec.rb
+++ spec/pgit/command/run_spec.rb
@@ -4,19 +4,19 @@ describe 'PGit::Command::Run' do
   describe '#run' do
     it 'raises a command not found exception if the item does not exist' do
       global_opts = {}
-      opts = []
+      opts = {}
       args = ['non_existent_command']
+      project = instance_double('PGit::CurrentProject')
 
-      fake_app = instance_double('PGit::Command::Application',
+      app = instance_double('PGit::Command::Application',
                                  commands: [],
                                  args: args,
                                  opts: opts,
-                                 global_opts: global_opts)
-      allow(PGit::Command::Application).to receive(:new).with(global_opts, opts, args).and_return(fake_app)
+                                 global_opts: global_opts,
+                                 current_project: project)
 
-      app = PGit::Command::Application.new(global_opts, opts, args)
       run = PGit::Command::Run.new(app)
-      error_message = "Command '#{args[0]}' does not exist. Run 'pgit command show' to see the available custom commands."
+      error_message = "Command '#{args[0]}' does not exist. Run 'pgit cmd show' to see the available custom commands."
 
       expect do
         run.execute!
@@ -26,19 +26,20 @@ describe 'PGit::Command::Run' do
     it 'calls execute on the command if it exists' do
       existent_name = 'existent_command'
       global_opts = {}
-      opts = []
+      opts = {}
       args = [existent_name]
 
       fake_command = instance_double('PGit::Command', name: existent_name)
       allow(fake_command).to receive(:execute)
-      fake_app = instance_double('PGit::Command::Application',
+      project = instance_double('PGit::CurrentProject')
+      app = instance_double('PGit::Command::Application',
                                  commands: [fake_command],
                                  args: args,
                                  opts: opts,
-                                 global_opts: global_opts)
-      allow(PGit::Command::Application).to receive(:new).with(global_opts, opts, args).and_return(fake_app)
+                                 global_opts: global_opts,
+                                 current_project: project
+                           )
 
-      app = PGit::Command::Application.new(global_opts, opts, args)
       run = PGit::Command::Run.new(app)
       run.execute!
 
