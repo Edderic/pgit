f7ff68b1afcfd1f4b3b6f7a67b4ef31537530d4f
Edderic Ugaddan <edderic@gmail.com>
2015-02-22 18:55:57 -0500

    [#86196738] updated test for project adder

--- spec/pgit/project/add_spec.rb
+++ spec/pgit/project/add_spec.rb
@@ -11,13 +11,14 @@ describe 'PGit::Project::Add' do
 
   describe '#execute!' do
     it 'instantiates a project and saves it' do
-      project = instance_double('PGit::Project')
+      project = instance_double('PGit::Project', save!: nil)
       app = instance_double('PGit::Project::Application',
                             exists?: false,
                             project: project)
       adder = instance_double('PGit::Project::InteractiveAdder',
-                              save!: nil,
-                              gather_missing_data: nil)
+                              execute!: nil,
+                              project: project,
+                              save!: nil)
       allow(PGit::Project::InteractiveAdder).to receive(:new).with(project).and_return(adder)
       add = PGit::Project::Add.new(app)
       allow(add).to receive(:puts).with("Successfully added the project!")
@@ -25,7 +26,7 @@ describe 'PGit::Project::Add' do
       add.execute!
 
       expect(adder).to have_received(:save!)
-      expect(adder).to have_received(:gather_missing_data)
+      expect(adder).to have_received(:execute!)
       expect(add).to have_received(:puts).with("Successfully added the project!")
     end
   end
