d2018d819250eea938837361393e4431a27a077d
Edderic Ugaddan <edderic@gmail.com>
2015-02-22 18:52:12 -0500

    [#86196738] make use of self accessors instead of using hashes in building a project. Cleaner.

--- lib/pgit/project/application.rb
+++ lib/pgit/project/application.rb
@@ -11,9 +11,9 @@ module PGit
       def initialize(global_opts, opts, args)
         @config = PGit::Configuration.new
         @project = PGit::Project.new(@config) do |p|
-          p["path"] = opts["path"]
-          p["api_token"] = opts["api_token"]
-          p["id"] = opts["id"]
+          p.path = opts["path"]
+          p.api_token = opts["api_token"]
+          p.id = opts["id"]
         end
       end
     end
