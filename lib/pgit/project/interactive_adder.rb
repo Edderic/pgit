973a19bbf69726910aa3e26b5092762f433017a2
Edderic Ugaddan <edderic@gmail.com>
2015-02-22 16:35:29 -0500

    [#86196738] decoupled InteractiveAdder and Project

--- lib/pgit/project/interactive_adder.rb
+++ lib/pgit/project/interactive_adder.rb
@@ -1,51 +1,25 @@
-require 'forwardable'
 require 'pgit'
 
 module PGit
   class Project
     class InteractiveAdder
-      extend Forwardable
-      def_delegators :@project, :api_token, :id, :save!
-      def_delegators :@app, :project, :projects
       attr_reader :project
 
-      def initialize(app)
-        @app = app
-        @project = app.project
+      def initialize(project)
+        @project = project
       end
 
       def execute!
-        gather_api_token
-        gather_id
+        ungiven_attrs.each do |attr|
+          puts "What's the project #{attr}?"
+          project.send("#{attr}=", STDIN.gets.chomp)
+        end
       end
 
       private
 
-      def gather(var_name, proper_value, question)
-        if @project.send(var_name) == proper_value
-          if projects.any?
-            question1 = 'There is at least one project saved in the configuration file. Do you want to reuse a project id? [Y/n]'
-            puts question1
-            if STDIN.gets.chomp.letter?('y')
-              ids = projects.map_with_index {|proj, i| "#{i}. path: #{proj.path}, id: #{proj.id}"}
-              ids.inject("Which one?") {|accum, item| accum + "#{item}\n"}
-            else
-            end
-          else
-            puts question
-            @project.send("#{var_name}=", STDIN.gets.chomp)
-          end
-        end
-      end
-
-      def gather_api_token
-        question = "What's the Pivotal Tracker API token (See http://pivotaltracker.com/profile)?"
-        gather('api_token', :no_api_token_provided, question)
-      end
-
-      def gather_id
-        question =  "What's the id of this project (i.e. https://www.pivotaltracker.com/n/projects/XXXX where XXXX is the id)?"
-        gather('id', :no_id_provided, question)
+      def ungiven_attrs
+        project.given_attrs.reject {|attr| project.send("given_#{attr}?")}
       end
     end
   end
--- spec/pgit/project/interactive_adder_spec.rb
+++ spec/pgit/project/interactive_adder_spec.rb
@@ -4,21 +4,16 @@ require 'byebug'
 describe 'PGit::Project::InteractiveAdder.new(proj)' do
   describe 'api_token does not exist' do
     it 'asks what the api token associated for the project is' do
-      question = "What's the Pivotal Tracker API token (See http://pivotaltracker.com/profile)?"
-      projects = []
-      project = instance_double('PGit::Project', api_token: :no_api_token_provided,
-                                              id: 12345)
+      question = "What's the project api_token?"
+      project = instance_double('PGit::Project', defaulted_attrs: [:api_token])
 
-      app = instance_double('PGit::Project::Application',
-                            projects: projects,
-                            project: project)
       allow_any_instance_of(PGit::Project::InteractiveAdder).to receive(:puts).with(question)
       api_token = 'SOMEAPITOKEN'
       response = instance_double('String', chomp: api_token)
       allow(STDIN).to receive(:gets).and_return(response)
       allow(project).to receive(:api_token=).with(api_token)
-      adder = PGit::Project::InteractiveAdder.new(app)
-      adder.gather_missing_data
+      adder = PGit::Project::InteractiveAdder.new(project)
+      adder.execute!
 
       expect(adder).to have_received(:puts).with(question)
       expect(project).to have_received(:api_token=).with(api_token)
@@ -27,48 +22,19 @@ describe 'PGit::Project::InteractiveAdder.new(proj)' do
 
   describe 'id does not exist' do
     it 'asks what the id associated for the project is' do
-      question = "What's the id of this project (i.e. https://www.pivotaltracker.com/n/projects/XXXX where XXXX is the id)?"
-      projects = []
-      project = instance_double('PGit::Project', id: :no_id_provided,
-                                              api_token: '12349798072thstueohoheu')
+      question = "What's the project id?"
+      project = instance_double('PGit::Project', defaulted_attrs: [:id])
       allow(project).to receive(:id=)
-      app = instance_double('PGit::Project::Application',
-                            projects: projects,
-                            project: project)
       allow_any_instance_of(PGit::Project::InteractiveAdder).to receive(:puts).with(question)
-      api_token = 'SOMEAPITOKEN'
-      response = instance_double('String', chomp: api_token)
+      id = 'someid'
+      response = instance_double('String', chomp: id)
       allow(STDIN).to receive(:gets).and_return(response)
-      allow(project).to receive(:api_token=).with(api_token)
-      adder = PGit::Project::InteractiveAdder.new(app)
-      adder.gather_missing_data
+      allow(project).to receive(:id=).with(id)
+      adder = PGit::Project::InteractiveAdder.new(project)
+      adder.execute!
 
       expect(adder).to have_received(:puts).with(question)
-      byebug
       expect(project).to have_received(:id=).with(id)
-    end
-
-    describe 'user already has a project somewhere' do
-      it 'offers user chance to just use the id that is already provided' do
-        question = 'There is at least one project saved in the configuration file. Do you want to reuse a project id? [Y/n]'
-        question1 = "Which one?\n  1. path: '/my/project1', id: 12345"
-        project1 = instance_double('PGit::Project',
-                                  path: '/my/project1',
-                                  id: 12345)
-        projects = [project1]
-        configuration = instance_double('PGit::Configuration', projects: projects)
-        project = instance_double('PGit::Project', id: :no_id_provided,
-                                                   api_token: '12349798072thstueohoheu')
-        allow(project).to receive(:configuration).and_return(configuration)
-        adder = PGit::Project::InteractiveAdder.new(project)
-        response = 'y'
-        allow(STDIN).to receive(:gets).and_return('y', 'y')
-        allow(adder).to receive(:puts)
-
-        adder.gather_missing_data
-        expect(adder).to have_received(:puts).with(question)
-        expect(adder).to have_received(:puts).with(question1)
-      end
     end
   end
 end
