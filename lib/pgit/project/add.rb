85d78ebb6d2f4ec96ba5200486d1f0d5bd2e94f6
Edderic Ugaddan <edderic@gmail.com>
2015-02-22 18:50:44 -0500

    [#86196738] interactive adder uses project and has #execute like all the other classes implementing the Command Pattern

--- lib/pgit/project/add.rb
+++ lib/pgit/project/add.rb
@@ -1,22 +1,22 @@
 require 'pgit'
-require 'forwardable'
 
 module PGit
   class Project
     class Add
       extend Forwardable
-      def_delegators :@app, :project
 
+      attr_reader :adder
       def initialize(app)
         @app = app
         raise PGit::Error::User, 'Project path already exists. See `pgit proj update --help.`' if app.exists?
 
-        @adder = PGit::Project::InteractiveAdder.new(project)
+        @adder = PGit::Project::InteractiveAdder.new(app.project)
       end
 
       def execute!
-        @adder.gather_missing_data
-        @adder.save!
+        adder.execute!
+        adder.save!
+
         puts "Successfully added the project!"
       end
     end
