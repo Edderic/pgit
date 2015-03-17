ef1ceee901663fa97e1cd6a0604c79a4b17877c8
Edderic Ugaddan <edderic@gmail.com>
2015-03-17 18:07:52 -0400

    [#90285028] completely use interactive gem to do the question and response handling

--- lib/pgit/project/reuse_api_token_adder.rb
+++ lib/pgit/project/reuse_api_token_adder.rb
@@ -14,50 +14,32 @@ module PGit
 
         reuse_question.ask_and_wait_for_valid_response do |reuse_response|
           if reuse_response.yes?
-            ask_which_one_and_wait_for_response
+            which_to_reuse_question.ask_and_wait_for_valid_response do |which_response|
+              if which_response.whole_number?
+                project.api_token = projects[which_response.to_i].api_token
+              end
+            end
           end
         end
       end
 
       private
 
-      def ask_which_one_and_wait_for_response
-        resp = 'invalid'
-        while resp == 'invalid' do
-          print_options
-          resp = response
-
-          if resp.index?
-            index = resp.to_i
-            project.api_token = projects[index - 1].api_token
-          elsif resp.cancel?
-          else
-            resp = 'invalid'
-          end
-        end
-      end
-
-      def print_options
-        puts "Which one? [#{indices}c]"
-        projects.each_with_index do |p, i|
-          puts "  #{i+1}.  #{p.path}: #{p.api_token}"
-        end
-      end
-
       def response
         STDIN.gets.chomp
-      end
-
-      def indices
-        str = ''
-        projects.length.times { |i| str += "#{i+1}/" }
-        str
       end
 
       def reuse_question
         Interactive::Question.new do |q|
           q.question = "Do you want to reuse an api token?"
           q.options = [:yes, :no]
+        end
+      end
+
+      def which_to_reuse_question
+        Interactive::Question.new do |q|
+          q.question = "Which one?"
+          q.options = [projects.map{|proj| "#{proj.path}: #{proj.api_token}"}, :cancel]
         end
       end
     end
--- spec/pgit/project/reuse_api_token_adder_spec.rb
+++ spec/pgit/project/reuse_api_token_adder_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'PGit::Project::ReuseApiTokenAdder' do
   describe '#execute' do
     describe 'there is already a project in the configuration file' do
-      it 'asks "Do you want to reuse an api token? [Y/n]"' do
+      it 'asks if you want to reuse an api token and uses that api_token if so' do
         api_token = 'someapitoken123'
         path = 'some/path'
         project_in_config = instance_double('PGit::Project',
@@ -13,108 +13,20 @@ describe 'PGit::Project::ReuseApiTokenAdder' do
         project = instance_double('PGit::Project')
         allow(project).to receive(:api_token=).with(api_token)
 
-        message = "Do you want to reuse an api token? [y/n]"
+        question1 = instance_double('Interactive::Question', :question= => nil,
+                                                             :options= => nil)
+        question2 = instance_double('Interactive::Question', :question= => nil,
+                                                             :options= => nil)
+        reuse_response = double('response', yes?: true)
+        which_response = double('response', whole_number?: true, to_i: 0)
+        allow(Interactive::Question).to receive(:new).and_return(question1, question2)
+        allow(question1).to receive(:ask_and_wait_for_valid_response).and_yield(reuse_response)
+        allow(question2).to receive(:ask_and_wait_for_valid_response).and_yield(which_response)
+
         adder = PGit::Project::ReuseApiTokenAdder.new(project, projects)
-        yes_response = instance_double('String', chomp: 'y')
-        cancel_response = instance_double('String', chomp: 'c')
-        allow_any_instance_of(Interactive::Question).to receive(:puts)
-        allow(adder).to receive(:puts)
-        allow(STDIN).to receive(:gets).and_return(yes_response, cancel_response)
-
-        expect_any_instance_of(Interactive::Question).to receive(:puts).with(message)
         adder.execute!
-      end
 
-      describe 'user responds "y"' do
-        it 'asks which one' do
-          api_token = 'someapitoken123'
-          path = '/some/path'
-          project_in_config = instance_double('PGit::Project',
-                                              api_token: api_token,
-                                              path: path)
-          projects = [project_in_config]
-          project = instance_double('PGit::Project')
-
-          message = "Do you want to reuse an api token? [Y/n]"
-          which_one_message = "Which one? [1/c]"
-          option_one_message = "  1.  #{path}: #{api_token}"
-
-          adder = PGit::Project::ReuseApiTokenAdder.new(project, projects)
-          response = instance_double('String', chomp: 'y')
-          cancel_response = instance_double('String', chomp: 'c')
-          allow(adder).to receive(:puts)
-          allow(STDIN).to receive(:gets).and_return(response, cancel_response)
-
-          adder.execute!
-          expect(adder).to have_received(:puts).with(which_one_message)
-          expect(adder).to have_received(:puts).with(option_one_message)
-        end
-
-        describe 'user responds "1"' do
-          it 'sets the project api_token to the api_token corresponding to the index' do
-            api_token = 'someapitoken123'
-            path = '/some/path'
-            project_in_config = instance_double('PGit::Project',
-                                                api_token: api_token,
-                                                path: path)
-            projects = [project_in_config]
-            project = instance_double('PGit::Project')
-            allow(STDIN).to receive(:gets)
-            project = instance_double('PGit::Project')
-
-            message = "Do you want to reuse an api token? [Y/n]"
-            which_one_message = "Which one? [1/c]"
-            option_one_message = "  1.  #{path}: #{api_token}"
-
-            adder = PGit::Project::ReuseApiTokenAdder.new(project, projects)
-            yes_response = instance_double('String', chomp: 'y')
-            cancel_response = instance_double('String', chomp: 'c')
-            allow(adder).to receive(:puts)
-            allow(STDIN).to receive(:gets).and_return(yes_response, cancel_response)
-            allow(project).to receive(:api_token=).with(api_token)
-
-            adder.execute!
-
-            expect(project).not_to have_received(:api_token=)
-          end
-        end
-
-        describe 'user responds with something else' do
-          it 'should reprint the options' do
-            api_token = 'someapitoken123'
-            path = '/some/path'
-            another_api_token = 'someotherapitoken'
-            another_path = '/some/other/path'
-            another_project_in_config = instance_double('PGit::Project',
-                                                api_token: another_api_token,
-                                                path: another_path)
-
-            project_in_config = instance_double('PGit::Project',
-                                                api_token: api_token,
-                                                path: path)
-
-            projects = [project_in_config, another_project_in_config]
-            project = instance_double('PGit::Project')
-
-            message = "Do you want to reuse an api token? [Y/n]"
-            which_one_message = "Which one? [1/2/c]"
-            option_one_message = "  1.  #{path}: #{api_token}"
-
-            adder = PGit::Project::ReuseApiTokenAdder.new(project, projects)
-            yes_response = instance_double('String', chomp: 'y')
-            cancel_response = instance_double('String', chomp: 'c')
-            unexpected_response = instance_double('String', chomp: 'a')
-
-            allow(adder).to receive(:puts)
-            allow(STDIN).to receive(:gets).and_return(yes_response, unexpected_response, cancel_response)
-            allow(project).to receive(:api_token=).with(api_token)
-
-            adder.execute!
-
-            expect(adder).to have_received(:puts).with(which_one_message).twice
-            expect(adder).to have_received(:puts).with(option_one_message).twice
-          end
-        end
+        expect(project).to have_received(:api_token=).with api_token
       end
     end
   end
