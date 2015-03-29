a449935846fb2637b94473cbb496e1489e37f9b8
Edderic Ugaddan <edderic@gmail.com>
2015-03-28 23:44:36 -0400

    [#90748872] wip: show summary view of iterations

--- lib/pgit/bilateral/story.rb
+++ lib/pgit/bilateral/story.rb
@@ -1,6 +1,32 @@
+require 'pgit'
+
 module PGit
-  module Interactive
+  module Bilateral
     class Story
+      include Interactive
+      def initialize
+        @iterations_obj = PGit::Pivotal::Iterations.new(scope: :current_backlog)
+        @iterations = @iterations_obj.get!
+      end
+
+      def execute!
+        which_question.ask do |which_response|
+        end
+      end
+
+      def stories
+        @iterations.inject([]) do |accum, iteration|
+          accum | iteration.stories
+        end
+      end
+
+      def which_question
+        Question.new do |which_question|
+          which_question.question = "Which story are you interested in?"
+          which_question.options = [stories, :back]
+          which_question.columns = [:index, :story_type, :estimate, :name, :current_state]
+        end
+      end
     end
   end
 end
--- spec/pgit/bilateral/story_spec.rb
+++ spec/pgit/bilateral/story_spec.rb
@@ -1,21 +1,37 @@
 require 'spec_helper'
 
-describe PGit::Interactive::Story do
+describe PGit::Bilateral::Story do
   describe '#execute!' do
     it 'show a list of stories for the current iteration' do
-      pending
-      project = instance_double('PGit::Project', id: 12345)
-      app = instance_double('PGit::Project::Application', project: project)
-      story = PGit::Interactive::Story.new(app)
-      story.execute!
+      project = instance_double('PGit::Project', id: 12345, api_token: 'someapitoken')
 
+      query = {scope: :current_backlog}
+      story_1 = instance_double('PGit::Story',
+                                story_type: 'chore',
+                                estimate: 2,
+                                name: 'some story',
+                                current_state: 'unstarted')
+      story_2 = instance_double('PGit::Story',
+                                story_type: 'feature',
+                                estimate: 3,
+                                name: 'some other story',
+                                current_state: 'unstarted')
+      stories = [story_1, story_2]
+      iteration_1 = double('PGit::Pivotal::Iteration', stories: stories)
+      iterations = [iteration_1]
+      iterations_obj = double('PGit::Pivotal::Iterations', get!: iterations)
+      allow(PGit::Pivotal::Iterations).
+        to receive(:new).with(query).and_return(iterations_obj)
+      config = double('config', :question= => nil, :options= => nil, :columns= => nil)
+      question = instance_double('Interactive::Question', ask: nil)
+      allow(Interactive::Question).to receive(:new).and_yield(config).and_return(question)
+      interactive_story = PGit::Bilateral::Story.new
+      interactive_story.execute!
 
-      stories =
-      Interactive::Question.new do |which_question|
-        which_question.question = "Which story are you interested in?"
-        which_question.options = [stories]
-      end
-      expect(story).to
+      expect(config).to have_received(:question=).with("Which story are you interested in?")
+      expect(config).to have_received(:options=).with([stories, :back])
+      expect(config).to have_received(:columns=).with([:index, :story_type, :estimate, :name, :current_state])
+      expect(question).to have_received(:ask)
     end
   end
 end
