9f318d723b8b869cc80e3edf5fb8a63c1a86792c
Edderic Ugaddan <edderic@gmail.com>
2015-06-27 22:12:30 -0400

    [#97951414] picking a story to branchify will succeed

--- lib/pgit/bilateral/handle_choose_story.rb
+++ lib/pgit/bilateral/handle_choose_story.rb
@@ -2,13 +2,15 @@ require 'pgit'
 
 module PGit
   module Bilateral
-    class HandleChooseStory < PGit::ResponseHandler
+    class HandleChooseStory < ResponseHandler
       def initialize(options)
         @response = options.fetch(:response)
         @stories = options.fetch(:stories)
-        @parent_question = options.fetch(:parent_question)
         @response_handlers = [PGit::Bilateral::HandleBack]
-        @question = _question
+        name_parser = PGit::StoryBranch::NameParser.new(chosen_story)
+        story_branch = PGit::StoryBranch.new(name_parser)
+
+        story_branch.start
       end
 
       private
@@ -17,23 +19,12 @@ module PGit
         @stories[@response.to_i]
       end
 
-      def string_options
-        ["start it", "start it and branch out"]
-      end
-
       def options(new_response)
         { response: new_response, stories: @stories, parent_question: @parent_question }
       end
 
-      def _question
-        Interactive::Question.new do |q|
-          q.question = "What would you like to do with #{Rainbow(chosen_story.name).bright}?"
-          q.options = [string_options, :back]
-        end
-      end
-
       def response_can_be_handled?
-        @response.whole_number?
+        false
       end
     end
   end
--- spec/pgit/bilateral/handle_choose_story_spec.rb
+++ spec/pgit/bilateral/handle_choose_story_spec.rb
@@ -1,58 +1,17 @@
 require 'spec_helper'
 
 describe PGit::Bilateral::HandleChooseStory do
-  describe '#execute' do
-    before { Rainbow.enabled = false }
+  it 'should create the branch' do
+    some_story = double('PGit::Story')
+    chosen_story = double('PGit::Story')
+    options = {response: '1', stories: [some_story, chosen_story]}
+    story_branch = double('PGit::StoryBranch', start: true)
 
-    it 'should not ask if the response is not a whole number' do
-      story = instance_double('PGit::Pivotal::Story', name: 'Bring me the passengers')
-      stories = [story]
-      response = double('Interactive::Response', whole_number_0?: false, whole_number?: false, to_i: 0)
-      what_to_do_message = "What would you like to do with #{story.name}?"
-      what_to_do_question = instance_double('Interactive::Question',
-                                            :question= => nil,
-                                            :options= => nil,
-                                            ask: nil
-                                           )
-      allow(Interactive::Question).to receive(:new).and_yield(what_to_do_question).and_return(what_to_do_question)
-      string_options = ["start it", "start it and branch out"]
-      back_handler = instance_double('PGit::Bilateral::HandleBack', execute!: nil)
-      parent_question = double('parent_question')
-      options = {response: response, stories: stories, parent_question: parent_question}
-      allow(PGit::Bilateral::HandleBack).to receive(:new).with(options).and_return(back_handler)
-      choose_story_handler = PGit::Bilateral::HandleChooseStory.new(options)
-      choose_story_handler.execute!
+    name_parser = double('PGit::StoryBranch::NameParser')
+    allow(PGit::StoryBranch::NameParser).to receive(:new).with(chosen_story).and_return(name_parser)
+    allow(PGit::StoryBranch).to receive(:new).with(name_parser).and_return(story_branch)
+    handle_choose_story = PGit::Bilateral::HandleChooseStory.new(options)
 
-      expect(what_to_do_question).not_to have_received(:ask)
-      expect(back_handler).not_to have_received(:execute!)
-    end
-
-    it 'should ask what the user would like to do with the story' do
-      story = instance_double('PGit::Pivotal::Story', name: 'Bring me the passengers')
-      stories = [story]
-      response = double('Interactive::Response', whole_number_0?: true, whole_number?: true, to_i: 0)
-      what_to_do_message = "What would you like to do with #{story.name}?"
-      what_to_do_question = instance_double('Interactive::Question',
-                                            :question= => nil,
-                                            :options= => nil,
-                                            ask: nil
-                                           )
-      allow(Interactive::Question).to receive(:new).and_yield(what_to_do_question).and_return(what_to_do_question)
-      string_options = ["start it", "start it and branch out"]
-      back_handler = instance_double('PGit::Bilateral::HandleBack', execute!: nil)
-      parent_question = double('parent_question')
-      what_to_do_response = instance_double('Interactive::Response')
-      options = {response: what_to_do_response, stories: stories, parent_question: parent_question}
-      allow(PGit::Bilateral::HandleBack).to receive(:new).with(options).and_return(back_handler)
-      response_options = { response: response, stories: stories, parent_question: parent_question }
-      allow(what_to_do_question).to receive(:ask).and_yield(what_to_do_response)
-      choose_story_handler = PGit::Bilateral::HandleChooseStory.new(response_options)
-      choose_story_handler.execute!
-
-      expect(what_to_do_question).to have_received(:options=).with([string_options,:back])
-      expect(what_to_do_question).to have_received(:question=).with(what_to_do_message)
-      expect(what_to_do_question).to have_received(:ask)
-      expect(back_handler).to have_received(:execute!)
-    end
+    expect(story_branch).to have_received(:start)
   end
 end
