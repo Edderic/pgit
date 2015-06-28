111fbb03565038a6800f27040d7e43fd05bae79d
Edderic Ugaddan <edderic@gmail.com>
2015-06-28 12:18:59 -0400

    [#97955196] Projects can take in an api_token (optional)

--- lib/pgit/pivotal/projects.rb
+++ lib/pgit/pivotal/projects.rb
@@ -3,6 +3,15 @@ require 'pgit'
 module PGit
   module Pivotal
     class Projects < CollectionRequest
+      def initialize(*args, &block)
+        @api_token = args.first[:api_token] unless args.empty?
+        yield self if block_given?
+      end
+
+      def api_token
+        @api_token ? @api_token : super
+      end
+
       def sublink
         'projects'
       end
--- spec/pgit/pivotal/projects_spec.rb
+++ spec/pgit/pivotal/projects_spec.rb
@@ -10,14 +10,32 @@ describe PGit::Pivotal::Projects do
       project = double('PGit::Pivotal::Project')
       allow(project).to receive(:hash=)
       get_request = double('get_request')
+      api_token = 'some_api_token_123'
       hashes_of_items = [{'name' => 'PGit', 'kind' => 'project' }]
       allow(PGit::Pivotal::Project).to receive(:new).and_yield(project).and_return(project)
+      allow_any_instance_of(PGit::Pivotal::CollectionRequest).to receive(:api_token).and_return(api_token)
       allow(JSON).to receive(:parse).with(get_request).and_return(hashes_of_items)
 
       projects = PGit::Pivotal::Projects.new
       allow(projects).to receive(:get_request).and_return(get_request)
       instantiated_projects = projects.get!
       expect(instantiated_projects).to include(project)
+      expect(projects.api_token).to eq api_token
+    end
+  end
+
+  describe 'with specific api_token' do
+    it 'should instantiate a bunch of PGit::Pivotal::Project instances only associated with the token' do
+      project = double('PGit::Pivotal::Project')
+      allow(project).to receive(:hash=)
+      get_request = double('get_request')
+      hashes_of_items = [{'name' => 'PGit', 'kind' => 'project' }]
+      allow(PGit::Pivotal::Project).to receive(:new).and_yield(project).and_return(project)
+      allow(JSON).to receive(:parse).with(get_request).and_return(hashes_of_items)
+
+      api_token = 'some_api_token_123'
+      projects = PGit::Pivotal::Projects.new(api_token: api_token)
+      expect(projects.api_token).to eq api_token
     end
   end
 
