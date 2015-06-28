2d299a4cb011c4f3801f40d5669d415359bd0fdb
Edderic Ugaddan <edderic@gmail.com>
2015-06-28 10:27:56 -0400

    [#97955196] move methods to parent class

--- lib/pgit/pivotal/collection_request.rb
+++ lib/pgit/pivotal/collection_request.rb
@@ -1,6 +1,21 @@
 module PGit
   module Pivotal
     class CollectionRequest < Pivotal::Request
+      def get!
+        hashes_of_items.map do |item_hash|
+          individual.new {|iter| iter.hash = item_hash}
+        end
+      end
+
+      private
+
+      def individual
+        self.class.to_s[0...-1].constantize
+      end
+
+      def hashes_of_items
+        JSON.parse(get_request)
+      end
     end
   end
 end
--- lib/pgit/pivotal/iterations.rb
+++ lib/pgit/pivotal/iterations.rb
@@ -7,20 +7,8 @@ module PGit
         @query = PGit::Pivotal::Request::Query.new(query)
       end
 
-      def get!
-        hashes_of_items.map do |item_hash|
-          PGit::Pivotal::Iteration.new {|iter| iter.hash = item_hash}
-        end
-      end
-
       def sublink
         "projects/#{@project_id}/iterations/#{@query}"
-      end
-
-      private
-
-      def hashes_of_items
-        JSON.parse(get_request)
       end
     end
   end
